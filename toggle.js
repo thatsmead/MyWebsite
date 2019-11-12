d92b007f6d98a914903cd018b75709f2f7809ac7
Adele <57577338+thatsmead@users.noreply.github.com>
2019-11-13 00:58:17 +0100

    Add files via upload

---
+++ toggle.js
@@ -0,0 +1,18 @@
+$("#pro1").hover(function () {
+    $("#name1").slideToggle("slow");
+});
+$("#pro2").hover(function () {
+    $("#name2").slideToggle("slow");
+});
+$("#pro3").hover(function () {
+    $("#name3").slideToggle("slow");
+});
+$("#pro4").hover(function () {
+    $("#name4").slideToggle("slow");
+});
+$("#pro5").hover(function () {
+    $("#name5").slideToggle("slow");
+});
+$("#pro6").hover(function () {
+    $("#name6").slideToggle("slow");
+});
